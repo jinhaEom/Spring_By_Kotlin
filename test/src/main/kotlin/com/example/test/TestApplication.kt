8c32c647e78a2f2c220f4b02ef24f23ec4ac9ad8
jinahaEom <umjin98@naver.com>
2022-07-07 21:00:03 +0900

    First SpringBoot Test

---
+++ test/src/main/kotlin/com/example/test/TestApplication.kt
@@ -0,0 +1,21 @@
+package com.example.test
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+class TestApplication
+
+fun main(args: Array<String>) {
+	runApplication<TestApplication>(*args)
+
+}
+
+@RestController
+class HelloController{
+	@GetMapping("/")
+	fun sayHello() = "Hello,World"
+
+}
