9b05d3ccf93af58b6608deef509b40c0306646e9
jinahaEom <umjin98@naver.com>
2022-08-19 14:42:03 +0900

    todo model java -> Kotlin 리팩토링

---
+++ fastcampus-todo-java/src/main/java/com/fastcampus/kotlinspring/todo/api/model/TodoListResponse.kt
@@ -0,0 +1,23 @@
+package com.fastcampus.kotlinspring.todo.api.model
+
+import com.fastcampus.kotlinspring.todo.domain.Todo
+import com.fasterxml.jackson.annotation.JsonIgnore
+
+data class TodoListResponse(
+    val items: List<Unit>,
+){
+    val size : Int
+    @JsonIgnore
+    get() = items.size
+
+
+    fun get(index: Int)= items[index]
+
+    companion object {
+        //코틀린에선 static 대신 companion
+
+        fun of (todoList : List<Todo>) =
+            TodoListResponse(todoList.map(TodoResponse::of))
+    }
+
+}
