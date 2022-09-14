099f79c77c6638083823dec9a7aebc496916bdff
jinahaEom <umjin98@naver.com>
2022-09-14 11:41:07 +0900

    Comment 삭제 api 구현(마무리)

--- issue-Service/issue-service2/src/main/kotlin/IS/issueService/web/CommentController.kt
+++ issue-Service/issue-service2/src/main/kotlin/IS/issueService/web/CommentController.kt
@@ -4,6 +4,7 @@ import IS.issueService.config.AuthUser
 import IS.issueService.model.CommentRequest
 import IS.issueService.model.CommentResponse
 import IS.issueService.service.CommentService
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
 
 
@@ -28,4 +29,14 @@ class CommentController(
         @PathVariable id: Long,
         @RequestBody request: CommentRequest,
     ) = commentService.edit(id, authUser.userId, request)
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT) //삭제 성공시
+    fun delete(
+        authUser: AuthUser,
+        @PathVariable issueId: Long,
+        @PathVariable id: Long,
+    ){
+        commentService.delete(issueId, id, authUser.userId)
+    }
 }
